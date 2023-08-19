9aa8719c3c38a3702b22b38a673ec29912a60edb
Shubham Vishwakarma <shubhamkv128@gmail.com>
2023-08-19 21:23:20 +0530

    chore: added helmet & compression middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from 'express'
+import compression from 'compression'
+import helmet from 'helmet'
+
 import {postsRouter} from './modules'
 
 const app = express()
 
+app.use(helmet())
+app.use(compression())
 app.use(express.json())
+
+app.disable('x-powered-by')
 
 app.get('/hello', (_, res) => {
 	res.json({
