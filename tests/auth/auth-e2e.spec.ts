3bd070da9cbb5e7d50ae9a7aca137eba83d99dc9
Shubham Vishwakarma <shubhamkv128@gmail.com>
2023-11-16 02:52:11 +0530

    feat: add assertions to check for the cookie header to a test case for login API

--- tests/auth/auth-e2e.spec.ts
+++ tests/auth/auth-e2e.spec.ts
@@ -9,6 +9,7 @@ import {LoginControllerResponse} from '../../src/modules/auth/types'
 import {CreateUserDto} from '../../src/modules/users/dto'
 import {User} from '../../src/modules'
 import {SuccessResponse} from '../../src/shared/types'
+import {REFRESH_TOKEN_COOKIE} from '../../src/modules/auth/constants'
 
 import {setup} from '../setup'
 
@@ -83,6 +84,10 @@ describe('POST /auth/login', () => {
 		const res = await request.post('/auth/login').send(loginData)
 
 		expect(res.status).toBe(200)
+		expect(res.headers['set-cookie'][0]).toBeDefined()
+		expect(res.headers['set-cookie'][0]).toContain(REFRESH_TOKEN_COOKIE)
+		expect(res.headers['set-cookie'][0]).toMatch(/httponly/i)
+		expect(res.headers['set-cookie'][0]).toMatch(/secure/i)
 		expect(res.body).toStrictEqual(expectedResponse)
 	})
 })
