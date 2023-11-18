d1edef17c49bc7823ef8d7a8932c9ff68baee5c3
Shubham Vishwakarma <shubhamkv128@gmail.com>
2023-11-18 22:37:04 +0530

    fix: changed request's user type & fixed refresh API

--- types/express/index.d.ts
+++ types/express/index.d.ts
@@ -1,9 +1,8 @@
 import {Request} from 'express'
+import {UserEntity} from '../../src/shared/modules/user'
 
 declare module 'express' {
 	interface Request {
-		user?: {
-			id: string
-		}
+		user?: UserEntity
 	}
 }
