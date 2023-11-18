ade704d049f0204201e1b84c8d741489571ddefc
Shubham Vishwakarma <shubhamkv128@gmail.com>
2023-11-18 16:05:09 +0530

    refactor: minor 'auth' and 'users' module code refactor

--- src/modules/users/middlewares/create-user-schema-validator.ts
+++ src/modules/users/middlewares/create-user-schema-validator.ts
@@ -1,20 +1,17 @@
 import type {NextFunction, Request, Response} from 'express'
-import {schemaValidator} from '../../../shared/middlewares'
 import {CreateUserJoiSchema} from '../joi-schema'
 
+import {schemaValidator} from '../../../shared/middlewares'
 import {buildErrorResponse} from '../../../utils'
 
-export function createUserSchemaValidator(
+export const createUserSchemaValidator = (
 	req: Request,
 	res: Response,
 	next: NextFunction
-) {
-	return schemaValidator({
+) =>
+	schemaValidator({
 		value: req.body,
 		schema: CreateUserJoiSchema,
 		onSuccess: next,
-		onFailure(error) {
-			return res.status(400).json(buildErrorResponse(error))
-		}
+		onFailure: (error) => res.status(400).json(buildErrorResponse(error))
 	})
-}
