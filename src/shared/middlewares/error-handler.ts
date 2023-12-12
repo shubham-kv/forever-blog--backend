e92bfdf8662bac426e98f63c27154d1863b352e0
Shubham Vishwakarma <shubhamkv128@gmail.com>
2023-12-12 15:55:29 +0530

    fix: patched the error handler middleware

--- src/shared/middlewares/error-handler.ts
+++ src/shared/middlewares/error-handler.ts
@@ -4,12 +4,12 @@ import {HttpError} from '../errors'
 import {buildErrorResponse} from '../../utils'
 import {SERVER_ERROR_MESSAGE} from '../constants'
 
-export const errorHandler: ErrorRequestHandler = (err, _req, res, next) => {
+export const errorHandler: ErrorRequestHandler = (err, _req, res) => {
 	const statusCode =
 		err instanceof HttpError ? err.status : res.statusCode ?? 500
 	const errorMessage =
 		err instanceof HttpError ? err.message : SERVER_ERROR_MESSAGE
 
 	res.status(statusCode).json(buildErrorResponse(errorMessage))
-	next()
+	console.error(err)
 }
