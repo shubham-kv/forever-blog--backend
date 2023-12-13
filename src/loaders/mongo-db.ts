41104901ca3ae7bd731a7165c9a9b4d52ba0a557
Shubham Vishwakarma <shubhamkv128@gmail.com>
2023-12-13 17:47:55 +0530

    refactor: updated mongo db loader to use the logger service

--- src/loaders/mongo-db.ts
+++ src/loaders/mongo-db.ts
@@ -1,33 +1,34 @@
 import mongoose from 'mongoose'
+
 import {appConfig} from '../configs'
+import {logger} from '../modules'
 
 const handleInitialConnectionError = (e: unknown) => {
-	console.error('Failed to setup a connection to mongo!')
-	console.error(e)
+	logger.error('Failed to setup a connection to mongo!')
+	logger.error(e)
 }
 
 const onConnectionOpen = () => {
-	console.log('MongoDB connection is now open!')
+	logger.info('MongoDB connection is now open!')
 }
 
 const onConnectionError = () => {
-	console.log('Some MongoDB connection error!')
+	logger.info('Some MongoDB connection error!')
 }
 
 const onDisconnection = () => {
-	console.log('MongoDB Disconnected')
+	logger.info('MongoDB Disconnected')
 }
 
-export const connectToMongo = async () => {
+export const connectToMongo = () => {
 	const {mongoUri} = appConfig
 
-	try {
-		await mongoose.connect(mongoUri)
+	mongoose
+		.connect(mongoUri)
+		.then(() => logger.info('Mongodb Connected!'))
+		.catch((e) => handleInitialConnectionError(e))
 
-		mongoose.connection.once('open', onConnectionOpen)
-		mongoose.connection.on('error', onConnectionError)
-		mongoose.connection.on('disconnected', onDisconnection)
-	} catch (e) {
-		handleInitialConnectionError(e)
-	}
+	mongoose.connection.once('open', onConnectionOpen)
+	mongoose.connection.on('error', onConnectionError)
+	mongoose.connection.on('disconnected', onDisconnection)
 }
