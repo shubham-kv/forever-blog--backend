90997886debbeb1c955e869f7b1a8cce479aab42
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2024-11-15 13:19:57 +0530

    refactor: refactor token config

--- src/configs/token.config.ts
+++ src/configs/token.config.ts
@@ -6,8 +6,8 @@ type TokenConfig = {
 }
 
 export const tokenConfig: TokenConfig = {
-	accessTokenSecret: process.env.ACCESS_TOKEN_SECRET ?? '',
-	accessTokenExpiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN ?? '',
-	refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET ?? '',
-	refreshTokenExpiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN ?? ''
+	accessTokenSecret: process.env.ACCESS_TOKEN_SECRET as string,
+	accessTokenExpiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN as string,
+	refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET as string,
+	refreshTokenExpiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN as string
 }
