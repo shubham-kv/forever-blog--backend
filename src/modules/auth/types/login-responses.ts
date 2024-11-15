5534eecdee653a97b677ec06038e6a59f7277f3a
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2024-11-15 13:19:58 +0530

    feat: added refresh token API

--- src/modules/auth/types/login-responses.ts
+++ src/modules/auth/types/login-responses.ts
@@ -1,8 +1,12 @@
-export type LoginControllerResponse = {
+export type AuthControllerResponse = {
 	token: string
 }
 
 export type LoginServiceResponse = {
 	accessToken: string
 	refreshToken: string
+}
+
+export type RefreshServiceResponse = {
+	accessToken: string
 }
