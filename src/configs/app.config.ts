95ffa5c8fac859ac62dc08c5dc5da705ecc9f02c
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2024-11-19 02:18:03 +0530

    chore: update default port to 4000

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -6,7 +6,7 @@ type AppConfigType = {
 }
 
 export const appConfig: AppConfigType = {
-	port: parseInt(process.env.PORT || '5000'),
+	port: parseInt(process.env.PORT || '4000'),
 	mongoUri: process.env.MONGO_URI,
 	frontendOrigin: process.env.FRONTEND_ORIGIN,
 	isProdEnv: process.env.NODE_ENV === 'production'
